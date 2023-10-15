3d6605cf4d59cd784758f638382c484b2c0d2fce
Sebastian Schleemilch <sebastian.schleemilch@gmail.com>
2023-10-15 22:25:16 +0200

    status quo

--- dotfiles/config/nvim/lua/config/package.lua
+++ dotfiles/config/nvim/lua/config/package.lua
@@ -78,7 +78,38 @@ local plugins = {
         "kylechui/nvim-surround",
         config = setup("nvim-surround")
     },
-    { "christoomey/vim-tmux-navigator" }
+    { "christoomey/vim-tmux-navigator" },
+    {
+        "williamboman/mason.nvim",
+        config = setup("mason"),
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = setup("mason"),
+    },
+    { 
+        "windwp/nvim-autopairs",
+        config = setup("nvim-autopairs"),
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        config = delegate("lsp"),
+        dependencies = {
+            {
+                "L3MON4D3/LuaSnip",
+                dependencies = {
+                    "rafamadriz/friendly-snippets"
+                }
+            },
+            {
+                "saadparwaiz1/cmp_luasnip",
+                "hrsh7th/cmp-nvim-lua",
+                "hrsh7th/cmp-nvim-lsp",
+                "hrsh7th/cmp-buffer",
+                "hrsh7th/cmp-path",
+            }
+        },
+    },
 }
 
 local opts = {}
