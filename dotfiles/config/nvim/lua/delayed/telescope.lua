a6ec85d7168e28e9deae159854d6716fdb0a323c
Sebastian Schleemilch <sebastian.schleemilch@posteo.net>
2023-12-06 23:02:52 +0100

    Deleted desktop

--- dotfiles/config/nvim/lua/delayed/telescope.lua
+++ dotfiles/config/nvim/lua/delayed/telescope.lua
@@ -1,10 +1,8 @@
 local builtin = require("telescope.builtin")
-local actions = require("telescope.actions")
 
 vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Telescope search files" })
 vim.keymap.set("n", "<C-g>", builtin.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<C-b>", builtin.buffers, { desc = "Telescope search buffers" })
-vim.keymap.set("n", "<C-h>", builtin.help_tags, { desc = "Telescope search help tags" })
 
 require("telescope").setup({
     defaults = {
