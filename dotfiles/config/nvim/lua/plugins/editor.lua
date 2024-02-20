cf78e8f704d4356a171e32738d013ed2db11a84e
Sebastian Schleemilch <sebastian.schleemilch@posteo.net>
2024-02-20 23:04:08 +0100

    Solid borders, removed nvim-notify

--- dotfiles/config/nvim/lua/plugins/editor.lua
+++ dotfiles/config/nvim/lua/plugins/editor.lua
@@ -36,6 +36,9 @@ return {
 		opts = {
 			oldfiles = {
 				cwd_only = true,
+            },
+			winopts = {
+				border = "single",
 			},
 		},
 	},
--- dotfiles/config/nvim/lua/plugins/ui.lua
+++ dotfiles/config/nvim/lua/plugins/ui.lua
@@ -34,33 +34,6 @@ return {
 			},
 		},
 	},
-	-- Better `vim.notify()`
-	{
-		"rcarriga/nvim-notify",
-		keys = {
-			{
-				"<leader>un",
-				function()
-					require("notify").dismiss({ silent = true, pending = true })
-				end,
-				desc = "Dismiss all Notifications",
-			},
-		},
-		opts = {
-			timeout = 3000,
-			max_height = function()
-				return math.floor(vim.o.lines * 0.75)
-			end,
-			max_width = function()
-				return math.floor(vim.o.columns * 0.75)
-			end,
-			on_open = function(win)
-				vim.api.nvim_win_set_config(win, { zindex = 100 })
-			end,
-			background_colour = "#24273a",
-			render = "compact",
-		},
-	},
 
 	-- statusline
 	{
@@ -202,6 +175,33 @@ return {
 				command_palette = true,
 				long_message_to_split = true,
 				inc_rename = true,
+			},
+			views = {
+				cmdline_popup = {
+					border = {
+						style = "single",
+					},
+				},
+				popup = {
+					border = {
+						style = "single",
+					},
+				},
+				cmdline_popupmenu = {
+					border = {
+						style = "single",
+					},
+				},
+				confirm = {
+					border = {
+						style = "single",
+					},
+				},
+				hover = {
+					border = {
+						style = "single",
+					},
+				},
 			},
 		},
         -- stylua: ignore
