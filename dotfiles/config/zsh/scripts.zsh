46b5a8af1f33ed00db76ae6d7bc252cc4399a311
Sebastian Schleemilch <sebastian.schleemilch@bmw.de>
2024-07-25 21:21:57 +0200

    Removed fsh, default is fine

--- dotfiles/config/zsh/scripts.zsh
+++ dotfiles/config/zsh/scripts.zsh
@@ -2,10 +2,6 @@ groot() {
     cd $(git rev-parse --show-toplevel)
 }
 
-set_fsh_theme() {
-    fast-theme XDG:catppuccin-macchiato
-}
-
 reset_nvim() {
     rm -rf ~/.config/nvim
     rm -rf ~/.local/share/nvim
